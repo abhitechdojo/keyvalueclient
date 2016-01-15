7379be99b3be9c93e3f6dddb5c29f562051818fa
Abhishek Srivastava <abhishek.srivastava@dealertrack.com>
2016-01-15 09:05:04 -0600

    fixed remoting problem

--- src/main/scala-2.11/com/abhi/akka/client/KeyValueClient.scala
+++ src/main/scala-2.11/com/abhi/akka/client/KeyValueClient.scala
@@ -1,8 +1,9 @@
 package com.abhi.akka.client
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorSystem}
 import akka.pattern.ask
 import akka.util.Timeout
+import com.abhi.akka.actors.KeyValueActor
 import com.abhi.akka.messages.{KeyValue, _}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -17,9 +18,9 @@ object KeyValueClient {
   def main(args: Array[String]) : Unit = {
     implicit val system = ActorSystem("LocalFileSystem")
     implicit val timeout = Timeout(2 seconds)
-    val keyValueActorRef = system.actorSelection("akka.tcp://keyvalue@127.0.0.1:2552")
-    keyValueActorRef ! SetRequest("foo", "bar")
-    (keyValueActorRef ? GetRequest("foo")).onSuccess({
+    val actor = system.actorOf(Props[KeyValueActor], "keyvalue-db")
+    actor ! SetRequest("foo", "bar")
+    (actor ? GetRequest("foo")).onSuccess({
       case x : KeyValue => println(s"got value ${x.value}")
     })
   }
